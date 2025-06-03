7e505efb354cce21053adf9f87d22b74448a308e
andro98 <andrewaacodo12@gmail.com>
2025-06-03 12:53:39 +0300

    Add Arabic, Logos, and env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,13 +17,9 @@ export const environment = {
   fineractPlatformTenantId: loadedEnv['fineractPlatformTenantId'] || 'default',
   fineractPlatformTenantIds: loadedEnv['fineractPlatformTenantIds'] || 'default',
   // For connecting to others servers running elsewhere update the base API URL
-  baseApiUrls:
-    loadedEnv['fineractApiUrls'] ||
-    'https://sandbox.mifos.community,https://demo.mifos.community,https://localhost:8443,' + window.location.origin,
+  baseApiUrls: 'https://197.165.161.185:8445',
   // For connecting to server running elsewhere set the base API URL
-  baseApiUrl:
-    loadedEnv['fineractApiUrl'] ||
-    (loadedEnv['fineractApiUrls']?.length > 0 ? loadedEnv['fineractApiUrls'].split(',')[0] : window.location.origin),
+  baseApiUrl: 'https://197.165.161.185:8445',
   allowServerSwitch: env.allow_switching_backend_instance,
   apiProvider: loadedEnv['apiProvider'] || '/fineract-provider/api',
   apiVersion: loadedEnv['apiVersion'] || '/v1',
@@ -39,9 +35,8 @@ export const environment = {
       'This system is for authorized use only. Unauthorized access will result in possible legal action. By accessing this system, you acknowledge that you are authorized to do so and that all data stored and processed here is confidential.',
     buttonText: 'Close'
   },
-  defaultLanguage: loadedEnv['defaultLanguage'] || 'en-US',
-  supportedLanguages:
-    loadedEnv['supportedLanguages'] || 'cs-CS,de-DE,en-US,es-MX,fr-FR,it-IT,ko-KO,lt-LT,lv-LV,ne-NE,pt-PT,sw-SW',
+  defaultLanguage: 'en-US',
+  supportedLanguages: 'en-US,ar-EG',
   preloadClients: loadedEnv['preloadClients'] || true,
 
   defaultCharDelimiter: loadedEnv['defaultCharDelimiter'] || ',',
